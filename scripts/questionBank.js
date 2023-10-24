29a6228efb5cd4a8ef1e4d5fddee68ff7b8cb0bc
Xenodius <xenodius@hotmail.com>
2023-10-24 20:18:15 -0300

    adding questions to questionBank JS

---
+++ scripts/questionBank.js
@@ -0,0 +1,27 @@
+let questions = [
+    {
+        prompt:"Which option is an HTML tag?",
+        option:["<p>","let",".class","npm"],
+        answer:"<p>"
+    },
+    {
+        prompt:"Which bracket is used for an array?",
+        option:["{}","()","[]","<>"],
+        answer:"[]"
+    },
+    {
+        prompt:"What CSS style is used to color a text?",
+        option:["background-color:","text-align:","padding:","color:"],
+        answer:"color:"
+    },
+    {
+        prompt:"Which of these options is not used in front-end web development?",
+        option:["HTML","SQL","CSS","Javascript"],
+        answer:"SQL"
+    },
+    {
+        prompt:"What HTML version is currently in use?",
+        option:["HTML-X","HTML3","CSS3","HTML5"],
+        answer:"HTML5"
+    }
+];
